e0dd023562a8c0e1d8b3b01eefd72e2ec0c5c372
jack <dowsanjack@gmail.com>
2015-01-05 18:55:20 +0530

    update and delete articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,31 @@
 class ArticlesController < ApplicationController
+
 	def new
-	
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 		#render plain: params[:article].inspect
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
 
@@ -18,6 +36,13 @@ class ArticlesController < ApplicationController
 
 	def index
 		@articles = Article.all
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private
